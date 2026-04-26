3c64dca628b07954f33eb51d2160144a90a2f0b9
it24103552 <it24103552@my.sliit.lk>
2026-04-26 10:51:10 +0530

    update URL

--- student-frontend/src/App.jsx
+++ student-frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
     try {
       const res = await axios.post(
-        "https://labbackend-d0ba.onrender.com",
+        "https://labbackend-d0ba.onrender.com/api/students",
         student
       );
       setMessage("Student added successfully ✅");
