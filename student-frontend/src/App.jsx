0bb0fa2b58d71466b30181fea1bad035cc727861
it24103552 <it24103552@my.sliit.lk>
2026-04-26 08:34:33 +0530

    Update API endpoint for student addition

--- student-frontend/src/App.jsx
+++ student-frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/students",
+        "https://labbackend-d0ba.onrender.com",
         student
       );
       setMessage("Student added successfully ✅");
